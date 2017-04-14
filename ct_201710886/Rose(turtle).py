8b213f5dae9249b459060ff6f5618aff8501e2f9
Kalmihyun <rkfalgus713@hanmail.net>
2017-04-14 21:06:29 +0900

    Add files via upload

---
+++ ct_201710886/Rose(turtle).py
@@ -0,0 +1,9 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> wn = turtle.Screen()
+>>> wn.setup(500,500)
+>>> t1 = turtle.Turtle()
+>>> wn . addshape("C:\\Users\\HOME\\Downloads\\rose.gif")
+>>> t1.shape("C:\\Users\\HOME\\Downloads\\rose.gif")
+>>> 

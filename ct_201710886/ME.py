ae85814c2a98b2c69d6208b66c334abbe7a47bb3
Kalmihyun <rkfalgus713@hanmail.net>
2017-04-16 23:58:49 +0900

    Add files via upload

---
+++ ct_201710886/ME.py
@@ -0,0 +1,10 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> wn = turtle.Screen()
+>>> wn . setup(500,500,0,0)
+>>> t1 = turtle. Turtle()
+>>> t1. shape("turtle")
+>>> wn . addshape("C:\\Users\\user\\Documents\\me.gif")
+>>> t1.shape("C:\\Users\\user\\Documents\\me.gif")
+>>> 
